d7a518921772d3583dabff5da3b58aececc08592
Jo Michell <jo.michell@uwe.ac.uk>
2024-02-23 17:29:19 +0000

    added a couple of simulations

--- steindl/simulations.py
+++ steindl/simulations.py
@@ -6,10 +6,14 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 # ----------------------------------------------------------
-# zeta = 0.77
+# Moderate stochastic/concentration balance, zeta = 0.77
 
+
+# create the simulation model
 sim1 = Steindl(num_firms = 1000, num_periods = 500, seed = 2)
 
+
+# set the model parameters
 sim1.set_params(
     alpha1  = 0.7,  # consumption out of income
     alpha2  = 0.1,  # consumption out of wealth
@@ -27,6 +31,7 @@ sim1.set_params(
     zeta    = 0.77   # size to revenue feedback 
 )
 
+# set the required initial variables
 sim1.set_ivars(
     Y    = 34,
     Y_hr = 27,
@@ -42,13 +47,21 @@ sim1.set_ivars(
     L    = 80 + 136,
 )
 
+# initialise the model (creates firms with stochastic variation etc.)
 sim1.initialise()
+
+# run the model
 sim1.run()
 
+# generate plots
 sim1.plot()
 
+
+# to inspect results manually, uncomment:
+# sim1.results
+
 # -------------------------------------------------------------
-# zeta = 0.5
+# Strong stochastic effect, zeta = 0.5
 
 # create a new sim based on sim1
 sim2 = Steindl(num_firms = 1000, num_periods = 500, seed = 1)
@@ -63,7 +76,7 @@ sim2.plot()
 
 
 # -------------------------------------------------------------
-# zeta = 0.7
+# Medium stochastic effect, zeta = 0.7
 
 sim3 = Steindl(num_firms = 1000, num_periods = 500, seed = 2)
 sim3.copy_init(sim1)
@@ -77,7 +90,7 @@ sim3.plot()
 
 
 # -------------------------------------------------------------
-# zeta = 0.9
+# Limited stochastic effect, strong concentration, zeta = 0.9
 
 sim4 = Steindl(num_firms = 1000, num_periods = 500, seed = 2)
 sim4.copy_init(sim1)
@@ -88,5 +101,32 @@ sim4.params.zeta = 0.9
 sim4.initialise()
 sim4.run()
 sim4.plot()
-)
+
  
+
+
+# -----------------------------------------------------------
+# Fully stochastic version, no concentration tendency
+
+
+sim5 = Steindl(num_firms = 1000, num_periods = 500, seed = 1)
+sim5.copy_init(sim1)
+
+sim5.params.zeta = 0
+
+sim5.initialise()
+sim5.run()
+sim5.plot()
+
+
+# -----------------------------------------------------------
+# 100% retained earnings, no retained earnings
+
+sim6 = Steindl(num_firms = 1000, num_periods = 500, seed = 1)
+sim6.copy_init(sim1)
+
+sim6.params.llambda = 1
+
+sim6.initialise()
+sim6.run()
+sim6.plot()
